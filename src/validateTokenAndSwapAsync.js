2009ef5ab97a57312a4279bc7d89bb4a2ae43a5e
Charles Feval <chfeval@microsoft.com>
2018-05-11 17:28:13 -0700

    Putting back url

--- src/validateTokenAndSwapAsync.js
+++ src/validateTokenAndSwapAsync.js
@@ -22,7 +22,7 @@ const getTokenExpiryAsync = async (token) => {
         tokenEntity = await retrieveEntityAsync(TOKEN_TABLE, token, "");
         return tokenEntity.expiration._;        
     } catch (error) {
-        throw Error(`Your token (${token}) does not exist.`);
+        throw Error(`Your token (${token}) does not exist. You need to sign-in first on ${FRONT_END_URL}`);
     }
 }
 
@@ -31,7 +31,7 @@ const validateTokenAsync = async (authorizationToken) => {
     console.log(`Token expiring on ${expiration} (Now is ${new Date()})`);
     if (expiration.getTime() < Date.now()) {
         console.error("Token is expired!");
-        throw Error(`Your session has expired`);
+        throw Error(`Your session has expired, you need to log back in on ${FRONT_END_URL}`);
     }
 }
 
